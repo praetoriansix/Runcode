3cf3087298c982f0208c1965936e47211ff1a0c3
praetoriansix <paul.m.williams82@gmail.com>
2020-05-24 18:32:17 -0400

    added function to print SHA1

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
-print("Hello, world")
+import hashlib
+
+print("Hello, World!")
+
+
+stringsha1 = hashlib.sha1(b"Hello, World!")
+hex_dig = stringsha1.hexdigest()
+#print(hex_dig)
